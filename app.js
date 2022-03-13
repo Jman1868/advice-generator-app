01bf5121d66f9f0c9e17fce1d74cdf9ba7ee4f55
FrostDragonGames <43161576+Jman1868@users.noreply.github.com>
2022-03-12 19:45:02 -0800

    Updated css.
    Updated css to almost semi complete state.

---
+++ app.js
@@ -0,0 +1,7 @@
+const adviceNumber = document.getElementById("adviceNumber");
+const advicetext = document.getElementById("adviceText");
+
+function changeText()
+{
+    alert("is this working?");
+}
