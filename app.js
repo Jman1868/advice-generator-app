4a4095f2c0feed17cc1c96307057185ba700aaf4
FrostDragonGames <43161576+Jman1868@users.noreply.github.com>
2022-03-19 22:05:38 -0700

    Finished Webapp, Added JS functionality.
    Finished the site. Added JS functionality. Created fetch Api to adviceslip. Removed debug code

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const adviceNumber = document.getElementById("adviceNumber");
-const advicetext = document.getElementById("adviceText");
+const adviceNumber = document.getElementById("adviceNumber"); //refrence to advicenumber
+const advicetext = document.getElementById("adviceText");    // refrence to advicetext
+const apiCall = 'https://api.adviceslip.com/advice';        //api endpoint
 
+//Called whenever we click the button
 function changeText()
-{
-    alert("is this working?");
+{   //Fetch call 
+    fetch(apiCall)
+        .then(response =>
+        {
+            return response.json(); //return data as json
+        }).then(data =>
+        {
+            adviceNumber.innerText = `ADVICE #${ data.slip.id }`;
+            advicetext.innerText =  `"${data.slip.advice}"`;
+        })
+    
+
 }
